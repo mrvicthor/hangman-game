4a9c1e589ba5c0e5ccf07592d76f381964aeb86c
Victor Eleanya <victoreleanya89@gmail.com>
2024-11-19 00:07:45 +0100

    design mobile and tab view for howToPlat.tsx

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,7 +1,50 @@
 import React from "react";
 
 const Cards = () => {
-  return <div>Cards</div>;
+  const cardsMetada = [
+    {
+      column: "01",
+      title: "choose a category",
+      description:
+        "First, choose a word category, like animals or movies. The computer then randomly selects a secret word from that topic and shows you blanks for each letter of the word.",
+    },
+    {
+      column: "02",
+      title: "guess letters",
+      description:
+        "Take turns guessing letters. The computer fills in the relevant blank spaces if your guess is correct. If it’s wrong, you lose some health, which empties after eight incorrect guesses.",
+    },
+    {
+      column: "03",
+      title: "win or lose",
+      description:
+        "You win by guessing all the letters in the word before your health runs out. If the health bar empties before you guess the word, you lose.",
+    },
+  ];
+
+  return cardsMetada.map((card, index) => (
+    <article
+      key={index}
+      className="bg-white rounded-3xl h-[11.5625rem] min-w-[20.25rem] md:h-[12.5rem] lg:h-[34.375rem] lg:w-[24rem] flex flex-col py-6 md:flex-row lg:flex-col md:items-center lg:pt-16 gap-6 px-6"
+    >
+      <div className="flex gap-4">
+        <p className="font-bold text-2xl md:text-7xl text-[#2463ff]">
+          {card.column}
+        </p>
+        <h2 className="text-2xl md:hidden md:text-3xl lg:text-4xl font-bold uppercase lg:tracking-widest text-[#261676]">
+          {card.title}
+        </h2>
+      </div>
+      <div>
+        <h2 className="text-2xl hidden md:block lg:text-center  md:text-3xl lg:text-4xl font-bold uppercase tracking-widest text-[#261676]">
+          {card.title}
+        </h2>
+        <p className=" text-[#887dc0] card-description lg:text-center lg:mt-6 text-base lg:text-[1.625rem]">
+          {card.description}
+        </p>
+      </div>
+    </article>
+  ));
 };
 
 export default Cards;
--- src/components/HowToPlay.tsx
+++ src/components/HowToPlay.tsx
@@ -1,29 +1,32 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import Cards from "./Cards";
 
 const HowToPlay = () => {
   return (
     <section className="container mx-auto px-6">
-      <div className="flex items-center gap-64 mt-24">
-        <div className="cursor-pointer h-[5.875rem] w-[5.875rem] rounded-full play-gradient relative flex items-center justify-center">
+      <div className="flex items-center gap-16 md:gap-64  mt-16">
+        <div className="cursor-pointer h-[2.5rem] w-[2.5rem] md:h-[4rem] md:w-[4rem] lg:h-[5.875rem] lg:w-[5.875rem] rounded-full play-gradient relative flex items-center justify-center">
           <Link
             href="/"
-            className="flex items-center justify-center h-[4.875rem] w-[4.875rem] rounded-full absolute top-0 play-gradient back-gradient "
+            className="flex items-center justify-center h-[1.5rem] w-[1.5rem] md:h-[3rem] md:w-[3rem] lg:h-[4.875rem] lg:w-[4.875rem] rounded-full absolute top-0 play-gradient back-gradient "
           >
             <Image
               src="/assets/images/icon-back.svg"
               width={24}
               height={24}
               alt="back icon"
-              className="animate-pulse"
+              className="animate-pulse h-[1rem] w-[1rem] md:h-[2rem] md:w-[2rem] lg:h-[3rem] lg:w-[3rem]"
             />
           </Link>
         </div>
-        <h1 className="heading-gradient text-9xl font-bold">How to Play</h1>
+        <h1 className="heading-gradient text-4xl md:text-6xl lg:text-9xl font-bold">
+          How to Play
+        </h1>
       </div>
-      <div>
-        <Cards>
+      <div className="flex flex-col gap-y-4 lg:flex-row lg:gap-x-4 mt-8">
+        <Cards />
       </div>
     </section>
   );
