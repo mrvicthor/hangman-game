9b6350a1c0b5423531d8354f290b863e261180de
Victor Eleanya <victoreleanya89@gmail.com>
2024-11-22 10:54:34 +0100

    feat(randomlyReplaceStringWithSpaces): added function to randomly replace text with spaces

--- src/app/board/[slug]/page.tsx
+++ src/app/board/[slug]/page.tsx
@@ -2,18 +2,24 @@ import React from "react";
 import { getBoardByCategory } from "@/lib/data";
 import BoardHeader from "@/components/BoardHeader";
 import Board from "@/components/Board";
-import { getRandomItemFromCategory } from "@/helpers";
+import {
+  getRandomItemFromCategory,
+  randomlyReplaceStringWithSpaces,
+} from "@/helpers";
 const page = async ({ params }: { params: Promise<{ slug: string }> }) => {
   const slug = (await params).slug;
   const title = decodeURIComponent(slug);
 
   const data = await getBoardByCategory(title);
   const randomSelectedCategory = getRandomItemFromCategory(JSON.parse(data));
+  const randomSelectedCategoryName = randomlyReplaceStringWithSpaces(
+    randomSelectedCategory.name
+  );
 
   return (
     <section className="container mx-auto px-6 flex flex-col gap-12">
       <BoardHeader title={title} />
-      <p>{randomSelectedCategory.name}</p>
+      <p>{randomSelectedCategoryName}</p>
       <div className="">
         <Board />
       </div>
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -5,3 +5,20 @@ type Category = {
 export const getRandomItemFromCategory = (arr: Category[]) => {
   return arr[Math.floor(Math.random() * arr.length)];
 };
+
+export const randomlyReplaceStringWithSpaces = (value: string) => {
+  const numToRemove =
+    value.length <= 4
+      ? Math.floor(Math.random() * 2) + 1
+      : Math.floor(Math.random() * 4) + 2;
+  const chars = value.split("");
+  const indicesToReplace = new Set();
+  while (indicesToReplace.size < numToRemove) {
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    indicesToReplace.add(randomIndex);
+  }
+  indicesToReplace.forEach((index) => {
+    chars[index as number] = " ";
+  });
+  return chars.join("");
+};
